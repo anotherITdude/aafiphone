54f37939185331c0deb9893cfb54fd56355be326
anotherITdude <abhi.baidya@gmail.com>
2023-11-22 11:03:23 +0100

    New: components, framer motion, ar page added

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,14 +1,12 @@
-import Image from "@/node_modules/next/image";
-import Logo from "../public/logo.svg";
 import Container from "@/components/Container";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-
+ 
 export default function Home() {
   return (
     <Container>
       <Navbar />
-      <Hero title="Hero Section" />
+      <Hero title="AAF Iphone" />
       <Hero title="Section 2" />
       <Hero title="Section 3" />
       <Hero title="Section 4" />
