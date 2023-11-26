ca87a1604dd519f09b60722063711a9bcc040d85
anotherITdude <abhi.baidya@gmail.com>
2023-11-26 14:44:54 +0100

    New: how to enter section arabic

--- locales/en.js
+++ locales/en.js
@@ -1,12 +1,27 @@
 const translations = {
+  //lucky Winner section
   'buy_and_savor': 'BUY, SAVOR AND ',
   'be_our_lucky_winner': 'BE OUR LUCKY WINNER!',
   'celebrate_national_day': 'Celebrate National Day with Al Ain Farms and seize the chance to win big!',
   'take_home_your_favourite': 'Take home your favourite products at your nearest Lulu Store and unwrap the excitement',
   'of_winning_the_brand_new_iphone': 'of winning the Brand-New iPhone 15 PRO MAX!',
   'register_now': 'Register Now',
+  //Win Iphone Section
   'win_iphone': 'Win iPhone',
-  '15_pro_max': '15PRO MAX'
+  '15_pro_max': '15PRO MAX',
+  //How to enter section
+  'How_to_enter': 'How to Enter',
+  'step_1': 'STEP 1',
+  'step_2': 'STEP 2',
+  'step_3': 'STEP 3',
+  'step_4': 'STEP 4',
+  'step_1_content': 'Purchase Al Ain Farms FRESH CHICKEN, FRESH MILK, LONG LIFE MILK, FRUIT MIX, FRESH EGGS, or FRESH YOGURT that have promotional QR Code on them.',
+  'step_2_content': 'Scan the QR Code using your Smartphone or Tablet.',
+  'step_3_content': 'Register on the microsite after scanning the QR Code & upload the Purchase Receipt.',
+  'step_4_content': 'Submit your entry to enter the draw.',
+  'disclaimer': '* This promotion is valid on select Al Ain Farms products. Products include: Fresh Chicken, Fresh Milk, Long Life Milk, Fruit Mix, Fresh Eggs, or Fresh Yoghurt. Head to your nearest Lulu stores to participate!'
+  
+
 };
 
 export default translations;
